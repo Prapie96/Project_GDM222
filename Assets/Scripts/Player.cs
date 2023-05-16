3dcccef99127b04c1267cb08b210d30ad2b9c51a
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-05-16 18:23:37 +0700

    fix bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,57 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Player : MonoBehaviour
-{   
+{
     private Vector2 targetPos;
     public float Yincrement;
 
     public float speed;
     public float maxHeight;
     public float minHeight;
-    public int health = 3; // HEath PLayer start = 3;
+    public int health = 3; // Health Player start = 3;
     public GameObject effect;
-    //Add Sound Move
+
+    // Add Sound Move
     AudioSource movesound;
+
     // Decrease Health
     public Text healthText;
+
     // Scene Over
     public GameObject gameOverPanel;
+
     void Start()
     {
-        //This code Make Player still Original Position;
         targetPos = transform.position;
         movesound = GetComponent<AudioSource>();
-     }
-
-    // Update is called once per frame
-    void Update()
-    {   if(health <= 0)
-        {
-            
-        
-             //show the game over 
-            gameOverPanel.SetActive(true);
-           
-        }
-          if (health >= 0)
-    {   
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
-        {
-            movesound.Play();
-            Instantiate(effect, transform.position, Quaternion.identity);
-            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
-        {
-            movesound.Play();
-            Instantiate(effect, transform.position, Quaternion.identity);
-            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
-        }
-
-        healthText.text = "Health : " + health;
     }
-       
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+
+            gameOverPanel.SetActive(true);
+
+        }
+
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+            {
+                MoveUp();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+            {
+                MoveDown();
+            }
+
+
+        }
+        if(health >= 0 )
+        {
+            healthText.text = "Health: " + health;
+        }
+
+    }
+
+    public void MoveUp()
+    {
+        if (transform.position.y < maxHeight)
+    {
+        movesound.Play();
+        Instantiate(effect, transform.position, Quaternion.identity);
+        targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+    }
+    }
+
+    public void MoveDown()
+    {
+        if(transform.position.y > minHeight){
+        movesound.Play();
+        Instantiate(effect, transform.position, Quaternion.identity);
+        targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+
     }
 }
--- Assets/Scripts/SkillBall.cs
+++ Assets/Scripts/SkillBall.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 public class SkillBall : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public float spiralRotationRate;
     public int bulletCount;
     public float bulletVelocity;
     public float timeBetweenBullets;
     public float cooldown;
-   
 
     private bool isReady = true;
 
@@ -21,23 +20,35 @@ public class SkillBall : MonoBehaviour
             StartCoroutine(ShootBurst());
         }
     }
+    public void ActivateSkill()
+{
+    if (isReady)
+    {
+        StartCoroutine(ShootBurst());
+    }
+}
+
 
     private IEnumerator ShootBurst()
     {
         isReady = false;
+        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
         float angle = 0f;
+        float radius = 0f;
 
         for (int i = 0; i < bulletCount; i++)
         {
-            angle += spiralRotationRate * Time.deltaTime;
-            float x = -Mathf.Cos(angle);
-            float y = -Mathf.Sin(angle);
+            angle += Mathf.PI * 2 * goldenRatio;
+            radius += bulletVelocity * timeBetweenBullets;
+
+            float x = radius * Mathf.Cos(angle);
+            float y = radius * Mathf.Sin(angle);
             Vector2 direction = new Vector2(x, y);
 
             GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.gravityScale = 0f;
-            rb.velocity = direction * bulletVelocity;
+            rb.velocity = direction.normalized * bulletVelocity;
 
             Destroy(bullet, 3f);
 
